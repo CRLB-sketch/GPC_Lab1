469ff5f3137b02a8094813e042f1c41c8f3f4717
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2022-08-29 14:49:09 -0700

    Implementando normal map

--- Engine3D.py
+++ Engine3D.py
@@ -7,7 +7,7 @@
 __author__ = "Cristian Laynez 201281"
 __status__ = "Student of Computer Science"
 
-# ! Engine 3D : Clase Principal para Iniciar el Lab
+# ! Engine 3D : Clase Principal para Iniciar el Render
 ######################################################################################
 
 from GlRender import *
@@ -20,32 +20,30 @@ height = 720
 
 rend = Renderer(width, height)
 
+rend.render_background("backgrounds/backgroundP1.bmp")
+
 # ! -----------------------------------------------------
 # PROBANDO NORMAL MAP
 
-# rend.dir_light = V3(-1, 0, 0)
+rend.dir_light = V3(-1, 0, 0)
 
-# rend.active_texture1 = Texture("example_models/model.bmp")
-# rend.normal_map = Texture("example_models/model_normal.bmp")
+rend.active_texture1 = Texture("example_models/model.bmp")
+rend.normal_map = Texture("example_models/model_normal.bmp")
 
-# rend.active_shader = shad.normal_map
-# rend.gl_load_model(
-#     "example_models/model.obj",
-#     translate=V3(-4, 0, -10),
-#     scale=V3(4, 4, 4),
-#     rotate=V3(0, 0, 0)
-# )
+rend.active_shader = shad.normal_map
+rend.gl_load_model(
+    "example_models/model.obj",
+    translate=V3(-4, 0, -10),
+    scale=V3(4, 4, 4),
+    rotate=V3(0, 0, 0)
+)
 
-# rend.active_shader = shad.gourad
-# rend.gl_load_model(
-#     "example_models/model.obj",
-#     translate = V3(4, 0, -10),
-#     scale = V3(4,4,4),
-#     rotate = V3(0,0,0)
-# )
+rend.active_shader = shad.gourad
+rend.gl_load_model(
+    "example_models/model.obj",
+    translate = V3(4, 0, -10),
+    scale = V3(4,4,4),
+    rotate = V3(0,0,0)
+)
 
-# rend.gl_finish("outputs/output.bmp")
-
-rend.render_background("backgrounds/backgroundP1.bmp")
-
-rend.gl_finish("outputs/test_background.bmp")
+rend.gl_finish("outputs/output.bmp")
--- _test.py
+++ _test.py
@@ -5,17 +5,45 @@ V2 = namedtuple('Point2', ['x', 'y'])
 V3 = namedtuple('Point3', ['x', 'y', 'z'])
 V4 = namedtuple('Point4', ['x', 'y', 'z', 'w'])
 
-tangent_matrix = [
-    [ 0.64755284, -0.4061203,  -0.41569427],
-    [-0.75890445, -0.26726016, -0.69428498],
-    [ 0.06884301,  0.87386401, -0.55794699],
-]
+# !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"""
++++++++++++++++++++++++++++++++++++++++++++
+VERTS1: Point3(x=3.7933844, y=-0.403388, z=-8.91156) - VERTS0: Point3(x=3.495088, y=-0.465472, z=-8.557888) + RES EDGE 1: [ 0.2982964  0.062084  -0.353672 ]
+VERTS2: Point3(x=3.7246804, y=0.0487572, z=-8.494516) - VERTS0: Point3(x=3.495088, y=-0.465472, z=-8.557888) + RES EDGE 2: [0.2295924 0.5142292 0.063372 ]
++ DELTA0: [0.48, 0.955, 0.0] - DELTA1: [0.489, 0.956, 0.0] - DELTA2: [0.484, 0.97, 0.0]
+SUBTRACT (DELTA RES): [0.009 0.001 0.   ] - [0.004 0.015 0.   ]
+VALUES: 0.009000000000000008 * 0.015000000000000013 - 0.0040000000000000036 * 0.0010000000000000009
+RES F: 7633.587786259529
+"""
+verts0 = V3(3.495088, -0.465472, -8.557888)
+verts1 = V3(3.7933844, -0.403388, -8.91156)
+verts2 = V3(3.7246804, 0.0487572, -8.494516)
 
-tex_normal = [-0.09888687, -0.06724307, 0.99282413]
+edge1 = mf.subtract_V3(verts1, verts0)
+edge2 = mf.subtract_V3(verts2, verts0)
+print(f"EDGE 1: {edge1} & EDGE 2: {edge2}")
 
-print(f"JAJAJAJ: {tangent_matrix[1]}")
-print(f"RES: {mf.multiply_m3x3_and_m3x1(tangent_matrix, tex_normal)}")
-# print(f"RES: {mf.multiply_matrixs([tangent_matrix, tangent_matrix])}")
+tex_coords0 = V3(0.48, 0.955, 0.0)
+tex_coords1 = V3(0.489, 0.956, 0.0)
+tex_coords2 = V3(0.484, 0.97, 0.0)
 
-f = 1 / float((-0.0020000000000000018 * 0.0010000000000000009 - -0.0020000000000000018 * 0.0010000000000000009))
-print(f"TESTING: {f}")
+delta_uv1 = mf.subtract_V3(tex_coords1, tex_coords0)
+delta_uv2 = mf.subtract_V3(tex_coords2, tex_coords0)
+print(f"SUBTRACT (DELTA RES): {delta_uv1} | {delta_uv2}")
+
+# !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# tangent_matrix = [
+#     [ 0.64755284, -0.4061203,  -0.41569427],
+#     [-0.75890445, -0.26726016, -0.69428498],
+#     [ 0.06884301,  0.87386401, -0.55794699],
+# ]
+
+# tex_normal = [-0.09888687, -0.06724307, 0.99282413]
+
+# print(f"JAJAJAJ: {tangent_matrix[1]}")
+# print(f"RES: {mf.multiply_m3x3_and_m3x1(tangent_matrix, tex_normal)}")
+# # print(f"RES: {mf.multiply_matrixs([tangent_matrix, tangent_matrix])}")
+
+# f = 1 / float((-0.0020000000000000018 * 0.0010000000000000009 - -0.0020000000000000018 * 0.0010000000000000009))
+# print(f"TESTING: {f}")
